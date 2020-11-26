7fa118cac8e8c384020f962e8e67f8ce161d0219
liyunteng <li_yunteng@163.com>
2020-11-26 16:51:32 +0800

    remove debug.mk

--- Makefile
+++ Makefile
@@ -3,5 +3,4 @@ include $(PROJECT_ROOT)/build/def.mk
 
 SUBDIRS = calc x a
 
-include $(PROJECT_ROOT)/build/debug.mk
 include $(PROJECT_ROOT)/build/subdir.mk
--- build/subdir.mk
+++ build/subdir.mk
@@ -57,9 +57,89 @@ showall: $(SUBDIRS_SHOW) show
 $(SUBDIRS_SHOW):
 	$(Q3)$(MAKE) -C $(patsubst %_show,%,$@) show || exit 1
 
+show:
+	@echo "=============== $(CURDIR) ==============="
+	@echo "BUILD_ENV          = " $(BUILD_ENV)
+	@echo "BUILD_VERBOSE      = " $(BUILD_VERBOSE)
+	@echo "BUILD_PWD          = " $(BUILD_PWD)
+	@echo "BUILD_OUTPUT       = " $(BUILD_OUTPUT)
+	@echo "D                  = " $(D)
+	@echo "Q1                 = " $(Q1)
+	@echo "Q2                 = " $(Q2)
+	@echo "Q3                 = " $(Q3)
+	@echo "O                  = " $(O)
+	@echo ""
+
+	@echo "SHELL              = " $(SHELL)
+	@echo "OS_TYPE            = " $(OS_TYPE)
+	@echo "CP                 = " $(CP)
+	@echo "RM                 = " $(RM)
+	@echo "MKDIR              = " $(MKDIR)
+	@echo ""
+
+	@echo "CURDIR             = " $(CURDIR)
+	@echo "MAKEFLAGS          = " $(MAKEFLAGS)
+	@echo "MAKEFILE_LIST      = " $(MAKEFILE_LIST)
+	@echo "MAKECMDGOALS       = " $(MAKECMDGOALS)
+	@echo "MAKEOVERRIDES      = " $(MAKEOVERRIDES)
+	@echo "MAKELEVEL          = " $(MAKELEVEL)
+	@echo "VPATH              = " $(VPATH)
+	@echo ""
+
+	@echo "OUT_ROOT           = " $(OUT_ROOT)
+	@echo "OUT_INCLUDE        = " $(OUT_INCLUDE)
+	@echo "OUT_BIN            = " $(OUT_BIN)
+	@echo "OUT_LIB            = " $(OUT_LIB)
+	@echo "OUT_OBJECT         = " $(OUT_OBJECT)
+	@echo "OUT_DEPEND         = " $(OUT_DEPEND)
+	@echo "OUT_CONFIG         = " $(OUT_CONFIG)
+	@echo ""
+
+	@echo "CROSS_COMPILE      = " $(CROSS_COMPILE)
+	@echo "CC                 = " $(CC)
+	@echo "CXX                = " $(CXX)
+	@echo "CPP                = " $(CPP)
+	@echo "AS                 = " $(AS)
+	@echo "LD                 = " $(LD)
+	@echo "AR                 = " $(AR)
+	@echo "NM                 = " $(NM)
+	@echo "STRIP              = " $(STRIP)
+	@echo "OBJCOPY            = " $(OBJCOPY)
+	@echo "OBJDUMP            = " $(OBJDUMP)
+	@echo "OBJSIZE            = " $(OBJSIZE)
+	@echo ""
+
+	@echo "CPPFLAGS           = " $(CPPFLAGS)
+	@echo "CFLAGS             = " $(CFLAGS)
+	@echo "CXXFLAGS           = " $(CXXFLAGS)
+	@echo "ASFLAGS            = " $(ASFLAGS)
+	@echo "LDFLAGS            = " $(LDFLAGS)
+	@echo "LOADLIBES          = " $(LOADLIBES)
+	@echo "LDLIBS             = " $(LDLIBS)
+	@echo "ARFLAGS            = " $(ARFLAGS)
+	@echo ""
+
+	@echo "MODE               = " $(MODE)
+	@echo "MODULE_ROOT        = " $(MODULE_ROOT)
+	@echo "MODULE_NAME        = " $(MODULE_NAME)
+	@echo "SUBDIRS            = " $(SUBDIRS)
+	@echo ""
+
 .PHONY: help
 help:
-	@echo "subdir: Build Sub Directories"
+	@echo "make <BUILD_ENV=[release|debug|debuginfo|map]> <CROSS_COMPILE=arm-linux-gnueabi-> <O=/opt/out> <V=[0|1|2|3]> <D=[0|1|2|3]> <show> <help>"
+	@echo ""
+	@echo "    BUILD_ENV           [release|debug|debuginfo|map] default is release"
+	@echo "    CROSS_COMPILE       cross compile toolchain"
+	@echo "    O                   output"
+	@echo "    V                   [0|1|2|3] verbose"
+	@echo "    D                   0 release | 1 debug | 2 gen debuginfo | 3 gen map"
+	@echo "    show                show current configuration"
+	@echo "    help                show this help"
+	@echo ""
+	@echo ""
+
+	@echo "subdir.mk : Build Sub Directories"
 	@echo ""
 	@echo "    MODULE_ROOT         the root directory of this module"
 	@echo "    MODULE_NAME         the name of this mudule"
