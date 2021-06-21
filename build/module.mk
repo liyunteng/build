8c2ac6c4e007c574b040a67e86382cc16da924a9
liyunteng <li_yunteng@163.com>
2021-06-22 00:45:13 +0800

    fix can't build

--- build/module.mk
+++ build/module.mk
@@ -4,7 +4,7 @@ MODULE_NAME ?= $(shell basename $(MODULE_ROOT))
 
 include $(PROJECT_ROOT)/build/def.mk
 include $(PROJECT_ROOT)/build/cmd.mk
-OBJOUT := $(OUT_OBJECT)$(dir $(subst $(PROJECT_ROOT),,$(shell pwd)))$(MODULE_NAME)
+OBJOUT := $(OUT_OBJECT)$(dir $(subst $(PROJECT_ROOT),,$(shell pwd)))
 
 ifeq ($(LOCAL_SO), y)
 LIBSO := $(OUT_LIB)/lib$(MODULE_NAME).so
@@ -15,42 +15,54 @@ LIBA  := $(OUT_LIB)/lib$(MODULE_NAME).a
 LIB   := $(LIBA) $(LIBSO)
 
 LOCAL_SRCS := $(shell find $(abspath .) -type f -name "*.c" -o -name "*.cc" -o -name "*.cpp" -o -name "*.s" -o -name "*.S")
-CPPFLAGS += -Iinclude
 LOCAL_CSRCS    := $(filter %.c,   $(LOCAL_SRCS))
 LOCAL_CPPSRCS  := $(filter %.cpp, $(LOCAL_SRCS))
 LOCAL_ASMSRCS  := $(filter %.S,   $(LOCAL_SRCS))
 LOCAL_ASMSRCS2 := $(filter %.s,   $(LOCAL_SRCS))
 LOCAL_CCSRCS   := $(filter %.cc,  $(LOCAL_SRCS))
 
-LOCAL_COBJS    := $(patsubst %.c,   $(OBJOUT)/%.o, $(LOCAL_CSRCS))
-LOCAL_CPPOBJS  := $(patsubst %.cpp, $(OBJOUT)/%.o, $(LOCAL_CPPSRCS))
-LOCAL_ASMOBJS  := $(patsubst %.S,   $(OBJOUT)/%.o, $(LOCAL_ASMSRCS))
-LOCAL_ASMOBJS2 := $(patsubst %.s,   $(OBJOUT)/%.o, $(LOCAL_ASMSRCS2))
-LOCAL_CCOBJS   := $(patsubst %.cc,  $(OBJOUT)/%.o, $(LOCAL_CCSRCS))
+LOCAL_COBJS    := $(patsubst $(MODULE_ROOT)/%.c,   $(OBJOUT)/%.o, $(LOCAL_CSRCS))
+LOCAL_CPPOBJS  := $(patsubst $(MODULE_ROOT)/%.cpp, $(OBJOUT)/%.o, $(LOCAL_CPPSRCS))
+LOCAL_ASMOBJS  := $(patsubst $(MODULE_ROOT)/%.S,   $(OBJOUT)/%.o, $(LOCAL_ASMSRCS))
+LOCAL_ASMOBJS2 := $(patsubst $(MODULE_ROOT)/%.s,   $(OBJOUT)/%.o, $(LOCAL_ASMSRCS2))
+LOCAL_CCOBJS   := $(patsubst $(MODULE_ROOT)/%.cc,  $(OBJOUT)/%.o, $(LOCAL_CCSRCS))
 
 LOCAL_OBJS := $(LOCAL_COBJS) $(LOCAL_CPPOBJS) $(LOCAL_ASMOBJS) $(LOCAL_ASMOBJS2) $(LOCAL_CCOBJS)
 
 LOCAL_CGCH   := $(patsubst %.h,%.h.gch,$(LOCAL_CHS))
 LOCAL_CPPGCH := $(patsubst %.h,%.h.gch,$(LOCAL_CPPHS))
-$(info OBJOUT = $(OBJOUT))
-$(info LOCAL_SRCS = $(LOCAL_SRCS))
-$(info LOCAL_CSRCS = $(LOCAL_CSRCS))
-$(info LOCAL_CPPSRCS = $(LOCAL_CPPSRCS))
-$(info LOCAL_ASMSRCS = $(LOCAL_ASMSRCS))
-$(info LOCAL_ASMSRCS2 = $(LOCAL_ASMSRCS2))
-$(info LOCAL_CCSRCS = $(LOCAL_CCSRCS))
+# $(info OBJOUT = $(OBJOUT))
+# $(info LOCAL_SRCS = $(LOCAL_SRCS))
+# $(info LOCAL_CSRCS = $(LOCAL_CSRCS))
+# $(info LOCAL_CPPSRCS = $(LOCAL_CPPSRCS))
+# $(info LOCAL_ASMSRCS = $(LOCAL_ASMSRCS))
+# $(info LOCAL_ASMSRCS2 = $(LOCAL_ASMSRCS2))
+# $(info LOCAL_CCSRCS = $(LOCAL_CCSRCS))
 
-$(info LOCAL_OBJS = $(LOCAL_OBJS))
-$(info LOCAL_COBJS = $(LOCAL_COBJS))
-$(info LOCAL_CPPOBJS = $(LOCAL_CPPOBJS))
-$(info LOCAL_ASMOBJS = $(LOCAL_ASMOBJS))
-$(info LOCAL_ASMOBJS2 = $(LOCAL_ASMOBJS2))
-$(info LOCAL_CCOBJS = $(LOCAL_CCOBJS))
+# $(info LOCAL_OBJS = $(LOCAL_OBJS))
+# $(info LOCAL_COBJS = $(LOCAL_COBJS))
+# $(info LOCAL_CPPOBJS = $(LOCAL_CPPOBJS))
+# $(info LOCAL_ASMOBJS = $(LOCAL_ASMOBJS))
+# $(info LOCAL_ASMOBJS2 = $(LOCAL_ASMOBJS2))
+# $(info LOCAL_CCOBJS = $(LOCAL_CCOBJS))
+
+LOCAL_INC ?= include
+CPPFLAGS += $(foreach dir, $(MODULE_ROOT)/$(LOCAL_INC), -I$(dir))
+LOCAL_EXPORT_DIR ?= $(LOCAL_INC)
+LOCAL_EXPORT_FILES := $(foreach dir, $(MODULE_ROOT)/$(LOCAL_EXPORT_DIR), $(shell find $(dir) -type f))
+LOCAL_EXPORTED_FILES := $(LOCAL_EXPORT_FILES:$(MODULE_ROOT)/$(LOCAL_EXPORT_DIR)/%=$(OUT_INCLUDE)/%)
+# $(info LOCAL_EXPORT_FILES = $(LOCAL_EXPORT_FILES))
+# $(info LOCAL_EXPORTED_FILES = $(LOCAL_EXPORTED_FILES))
 
 OBJ_MKDIR = if [ ! -d $(dir $@) ]; then mkdir -p $(dir $@); fi
 
 
-all: SUB_MODULE_BUILD $(LIB)
+all: header SUB_MODULE_BUILD $(LIB)
+
+header: $(LOCAL_EXPORTED_FILES)
+$(LOCAL_EXPORTED_FILES): $(OUT_INCLUDE)/% : $(LOCAL_EXPORT_DIR)/%
+	$(Q)$(OBJ_MKDIR)
+	$(call cmd_cp,$(MODULE_NAME),$<,$@)
 
 SUB_MODULE_BUILD: $(MODULE_y)
 	$(Q)for dir in $(MODULE_y); \
